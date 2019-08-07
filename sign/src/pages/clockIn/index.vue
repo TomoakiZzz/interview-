bde314b288843e7851acaae30d32f660bdbe60fc
myl <1244282340@qq.com>
2019-08-07 10:58:59 +0800

    定位

--- sign/src/pages/clockIn/index.vue
+++ sign/src/pages/clockIn/index.vue
@@ -1,101 +1,114 @@
 <template>
   <div class="clockIn">
     <div class="map">
-     <map 
-      id="map" 
-      show-location 
+      <map id="map" 
+      :markers="markers" 
+      :longitude="longitude" 
+      :latitude="latitude" 
+      show-location
       ></map>
+      <cover-view class="location" @click="orientation">
+        <button>定位</button>
+      </cover-view>
     </div>
-    <div class="location">
-      <span><img src="../../imgs/people.png" alt="" class="img"></span>
-      <b><img src="../../imgs/location.png" 
-          alt="" 
-          class="img"
-          @click="clicklocation"
-      ></b>
+    <div class="locations">
+      <span>
+        <img src="../../imgs/people.png" alt class="img" />
+      </span>
+      <b>
+        <img src="../../imgs/location.png" alt class="img" @click="clicklocation" />
+      </b>
     </div>
     <div class="bottom">
-      <button @click="addinterview">添加面试</button>
+      <button @click="addinterview" class="addinterview">添加面试</button>
     </div>
   </div>
 </template>
 
 <script>
 // Use Vuex
-import store from './store'
+import { mapState, mapActions } from "vuex";
 
 export default {
-  data(){
+  data() {
     return {
-
-    }
+      markers: []
+    };
   },
   computed: {
-    count () {
-      return store.state.count
-    }
+    ...mapState({
+      longitude: state => state.clockIn.longitude,
+      latitude: state => state.clockIn.latitude
+    })
   },
   methods: {
-    addinterview(e){//添加面试
-      console.log(e)
+    ...mapActions({
+      orientation: "clockIn/getLocation"
+    }),
+    addinterview(e) {
+      //添加面试
+      console.log(e);
     },
-    regionchange: (e) => {
-      console.log(e)
-   },
-   clicklocation:(e)=>{
-     wx.navigateTo({
-       url: '/pages/personal/main',
-       success: (result)=>{
-         
-       },
-       fail: ()=>{},
-       complete: ()=>{}
-     });
-   }
+    regionchange: e => {
+      console.log(e);
+    },
+    clicklocation: e => {
+      wx.navigateTo({
+        url: "/pages/personal/main",
+        success: result => {},
+        fail: () => {},
+        complete: () => {}
+      });
+    }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-.clockIn{
-  width:100%;
-  height:100%;
+.clockIn {
+  width: 100%;
+  height: 100%;
   background: skyblue;
 }
-.map{
-  width:100%;
-  height:100%;
-}
-#map{
-  width:100%;
-  height:100%;
-}
-.location{
-  width:100%;
+.location {
   position: fixed;
-  top:80%;
+  top: 30rpx;
+  left: 30rpx;
+}
+.map {
+  width: 100%;
+  height: 100%;
+}
+#map {
+  width: 100%;
+  height: 100%;
+}
+.locations {
+  width: 100%;
+  position: fixed;
+  top: 80%;
   display: flex;
-  span{
-    flex:8;
-    .img{
-      width:80rpx;
-      height:80rpx;
+  span {
+    flex: 8;
+    .img {
+      width: 80rpx;
+      height: 80rpx;
       margin-left: 40rpx;
     }
   }
-  b{
-    .img{
-      width:80rpx;
-      height:80rpx;
+  b {
+    .img {
+      width: 80rpx;
+      height: 80rpx;
       margin-right: 40rpx;
     }
   }
 }
-button{
-  width:100%;
+.addinterview {
+  width: 100%;
   background: #000;
-  color:#fff;
-  bottom:0;
+  color: #fff;
+  bottom: 0;
   position: fixed;
 }
 </style>
---
+++ sign/src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import CreateLogger from 'vuex/dist/logger';
+
+// 引入store子模块
+import clockIn from './modules/clockIn';
+
+Vue.use(Vuex);
+
+
+export default new Vuex.Store({
+  modules: {
+    clockIn
+  },
+  plugins: [CreateLogger()]
+})
